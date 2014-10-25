21ab157c739a650e24dcaa9244a33444bccb6e0b
Alex Yao <alexyaoyang@hotmail.com>
2014-10-25 22:18:24 +0800

    base done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 function layOutDay(events){
-	console.log(events);
+	for(var i=0;i<events.length;i++){
+		console.log(events[i].start+" "+events[i].end);
+
+	}
+}
+
+//automate time creation
+function initialize(){
+	var AMPM = ' AM';
+	for(var i=9;i<22;i++){
+		var hour = i%12;
+		if(i%12==0) { AMPM = ' PM';hour=12; }
+		var mainTime = $('<span/>',{
+			class:'bold big-font',
+			html:hour+AMPM+'<br>'
+		});
+		var halfTime = $('<span/>',{
+			class:'gray',
+			html:hour+':30<br>'
+		});
+		mainTime.appendTo('#time-container');
+		if(i!=21) { halfTime.appendTo('#time-container'); }
+	}
 }
